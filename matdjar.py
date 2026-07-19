b405a86161ea8b952bfeb375e05f8635e9c719a1
aissa1401r-design <aissa1401r@gmail.com>
2026-07-19 20:35:15 +0200

    Update matdjar.py

--- matdjar.py
+++ matdjar.py
@@ -7,7 +7,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = os.environ.get("BOT_TOKEN")
 
 application = ApplicationBuilder().token(TOKEN).build()
-app = Flask(name)
+app = Flask(__name__)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("مرحبا! البوت راه خدام ✅")
@@ -27,6 +27,6 @@ async def webhook():  # <-- درناها async
 def home():
     return "Bot is alive"
 
-if name == "main":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
