02f933c4051e169ab53725cc23edcac9f5ec320d
aissa1401r-design <aissa1401r@gmail.com>
2026-07-22 10:14:29 +0200

    Update matdjar.py

--- matdjar.py
+++ matdjar.py
@@ -1,23 +1,24 @@
 import os
 from flask import Flask, request
-from telegram import Bot, Update
-from telegram.ext import Dispatcher, CommandHandler
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.environ.get("BOT_TOKEN")
-bot = Bot(TOKEN)
+PORT = int(os.environ.get("PORT", 10000))
+
 app = Flask(__name__)
+application = Application.builder().token(TOKEN).build()
 
-def start(bot, update):
-    bot.send_message(chat_id=update.effective_chat.id, text="مرحبا! البوت راه خدام ✅")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="مرحبا ! البوت راه خدام ✅")
 
-dispatcher = Dispatcher(bot, None)
-dispatcher.add_handler(CommandHandler("start", start))
+application.add_handler(CommandHandler("start", start))
 
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
-    update = Update.de_json(request.get_json(force=True), bot)
-    dispatcher.process_update(update)
+    update = Update.de_json(request.get_json(force=True), application.bot)
+    application.update_queue.put_nowait(update)
     return "ok"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=PORT)
