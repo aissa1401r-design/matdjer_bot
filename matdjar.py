76406a9655541fd6efcca7d2a9140f41ed32b288
aissa1401r-design <aissa1401r@gmail.com>
2026-07-22 10:39:46 +0200

    Update matdjar.py

--- matdjar.py
+++ matdjar.py
@@ -1,27 +1,23 @@
 import os
-import asyncio
-from flask import Flask, request
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from flask import Flask, request, jsonify
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, CommandHandler
 
 TOKEN = os.environ.get("BOT_TOKEN")
-PORT = int(os.environ.get("PORT", 10000))
-URL = os.environ.get("RENDER_EXTERNAL_URL")
 
 app = Flask(__name__)
+bot = Bot(TOKEN)
+dispatcher = Dispatcher(bot, None, workers=0)
 
-# نخدمو الابليكايشن هكا في v20
-application = Application.builder().token(TOKEN).build()
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="البوت راه خدام ✅")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="مرحبا ! البوت راه خدام ✅")
-
-application.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("start", start))
 
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
-    update = Update.de_json(request.get_json(force=True), application.bot)
-    asyncio.run(application.process_update(update)) # هاذي هي المهمة
+    update = Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
     return "ok"
 
 @app.route("/")
@@ -29,6 +25,4 @@ def home():
     return "Bot is running"
 
 if __name__ == "__main__":
-    # ندير SetWebhook قبل ما نطلع
-    asyncio.run(application.bot.set_webhook(url=f"{URL}/{TOKEN}"))
-    app.run(host="0.0.0.0", port=PORT)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
